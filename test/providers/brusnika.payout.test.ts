47df8a55bffc1d27ea1ec3053b4e39298e75c344
Vadim Gerasimov <angryvadik@gmail.com>
2026-04-09 17:36:19 +0000

    feat(8pay): brusnika payout skip_processing_url

--- test/providers/brusnika.payout.test.ts
+++ test/providers/brusnika.payout.test.ts
@@ -1,5 +1,5 @@
 import * as common from "@/common";
-import { BrusnikaPayment } from "@/provider_mocks/brusnika";
+import { BrusnikaPayout } from "@/provider_mocks/brusnika";
 import { payoutSuite } from "@/provider_mocks/brusnika";
 import {
   callbackFinalizationSuite,
@@ -8,8 +8,9 @@ import {
 } from "@/suite_interfaces";
 import { providers } from "@/settings_builder";
 import { test } from "@/test_context";
-import { assert } from "vitest";
+import { assert, describe } from "vitest";
 import { delay } from "@std/async";
+import { CONFIG } from "@/config";
 
 const CURRENCY = "UZS";
 
@@ -23,103 +24,173 @@ let brusnikaSuite = () => {
   return { ...suite, settings };
 };
 
-callbackFinalizationSuite(brusnikaSuite);
-statusFinalizationSuite(brusnikaSuite);
+describe
+  .runIf(CONFIG.in_project("8pay"))
+  .concurrent("brusnika 8pay payout", () => {
+    callbackFinalizationSuite(brusnikaSuite);
+    statusFinalizationSuite(brusnikaSuite);
 
-test.concurrent("brusnika no balance decline", async ({ ctx }) => {
-  await ctx.track_bg_rejections(async () => {
-    let merchant = await ctx.create_random_merchant();
-    await merchant.cashin("UZS", common.amount / 100);
-    let settings = providers("UZS", {
-      ...BrusnikaPayment.settings(ctx.uuid),
-      wrapped_to_json_response: true,
+    test.concurrent("brusnika no balance decline", async ({ ctx }) => {
+      await ctx.track_bg_rejections(async () => {
+        let merchant = await ctx.create_random_merchant();
+        await merchant.cashin("UZS", common.amount / 100);
+        let settings = providers("UZS", {
+          ...BrusnikaPayout.settings(ctx.uuid),
+          wrapped_to_json_response: true,
+        });
+        settings["payout_providers_card"] = true;
+        await merchant.set_settings(settings);
+        let brusnika = ctx.mock_server(
+          BrusnikaPayout.mock_params_uzs(ctx.uuid),
+        );
+        brusnika.queue(BrusnikaPayout.no_balance_handler());
+        let notification = merchant.queue_notification((callback) => {
+          assert.strictEqual(
+            callback.status,
+            "declined",
+            "declined notification",
+          );
+        });
+        let response = await merchant
+          .create_payment(common.paymentRequest("UZS"))
+          .then((p) => p.followFirstProcessingUrl());
+        let err = await response.as_error();
+        err.assert_message(
+          "gateway response error: Not enough money on balance",
+        );
+        await notification;
+      });
     });
-    settings["payout_providers_card"] = true;
-    await merchant.set_settings(settings);
-    let brusnika = ctx.mock_server(BrusnikaPayment.mock_params_uzs(ctx.uuid));
-    brusnika.queue(BrusnikaPayment.no_requisites_handler());
-    let notification = merchant.queue_notification((callback) => {
-      assert.strictEqual(callback.status, "declined", "declined notification");
-    });
-    let response = await merchant
-      .create_payment(common.paymentRequest("UZS"))
-      .then((p) => p.followFirstProcessingUrl());
-    let err = await response.as_error();
-    err.assert_message(
-      "gateway response error: Not found available payment details",
-    );
-    await notification;
-  });
-});
 
-test.concurrent("brusnika pending if 500", async ({ ctx }) => {
-  await ctx.track_bg_rejections(async () => {
-    let merchant = await ctx.create_random_merchant();
-    await merchant.cashin("UZS", common.amount / 100);
-    let settings = providers("UZS", {
-      ...BrusnikaPayment.settings(ctx.uuid),
-      wrapped_to_json_response: true,
+    test.concurrent("brusnika payout skip_processing_url", async ({ ctx }) => {
+      await ctx.track_bg_rejections(async () => {
+        let merchant = await ctx.create_random_merchant();
+        await merchant.cashin("UZS", common.amount / 100);
+        let settings = providers("UZS", {
+          ...BrusnikaPayout.settings(ctx.uuid),
+          wrapped_to_json_response: true,
+        });
+        settings["payout_providers_card"] = true;
+        settings["gateways"]["skip_processing_url"] = true;
+        await merchant.set_settings(settings);
+        let brusnika = ctx.mock_server(
+          BrusnikaPayout.mock_params_uzs(ctx.uuid),
+        );
+        let payout = new BrusnikaPayout();
+        brusnika.queue(payout.create_handler("in_progress"));
+        let notification = merchant.queue_notification((callback) => {
+          assert.strictEqual(
+            callback.status,
+            "declined",
+            "declined notification",
+          );
+        });
+        let response = await merchant.create_payout({
+          product: "Tests PayOut",
+          amount: common.amount,
+          currency: "UZS",
+          card: {
+            pan: common.visaCard,
+          },
+          bank_account: {
+            bank_name: "sberbank",
+            requisite_type: "card",
+          },
+          order_number: "123456789",
+          customer: {
+            email: common.email,
+            ip: common.ip,
+            first_name: common.firstName,
+            last_name: common.lastName,
+            phone: "+" + common.phoneNumber,
+          },
+        });
+        assert.strictEqual(response?.payout?.status, "pending");
+        await notification;
+      });
     });
-    settings["payout_providers_card"] = true;
-    await merchant.set_settings(settings);
-    let brusnika = ctx.mock_server(BrusnikaPayment.mock_params_uzs(ctx.uuid));
-    brusnika.queue(common.nginx500);
-    let notification = merchant.queue_notification((callback) => {
-      assert.strictEqual(callback.status, "declined", "declined notification");
-    });
-    let response = await merchant
-      .create_payout({
-        ...common.payoutRequest("UZS"),
-        extra_return_param: "card",
-        card: { pan: common.visaCard },
-        bank_account: {
-          bank_name: "sberbank",
-          requisite_type: "card",
-        },
-      })
-      .then((p) => p.followFirstProcessingUrl());
-    let err = await response.as_error();
-    err.assert_message(
-      "gateway response error: Not found available payment details",
-    );
-    await notification;
-  });
-});
 
-test.concurrent("brusnika pending if timeout", async ({ ctx }) => {
-  await ctx.track_bg_rejections(async () => {
-    let merchant = await ctx.create_random_merchant();
-    await merchant.cashin("UZS", common.amount / 100);
-    let settings = providers("UZS", {
-      ...BrusnikaPayment.settings(ctx.uuid),
-      wrapped_to_json_response: true,
+    test.concurrent("brusnika pending if 500", async ({ ctx }) => {
+      await ctx.track_bg_rejections(async () => {
+        let merchant = await ctx.create_random_merchant();
+        await merchant.cashin("UZS", common.amount / 100);
+        let settings = providers("UZS", {
+          ...BrusnikaPayout.settings(ctx.uuid),
+          wrapped_to_json_response: true,
+        });
+        settings["payout_providers_card"] = true;
+        await merchant.set_settings(settings);
+        let brusnika = ctx.mock_server(
+          BrusnikaPayout.mock_params_uzs(ctx.uuid),
+        );
+        brusnika.queue(common.nginx500);
+        let notification = merchant.queue_notification((callback) => {
+          assert.strictEqual(
+            callback.status,
+            "declined",
+            "declined notification",
+          );
+        });
+        let response = await merchant
+          .create_payout({
+            ...common.payoutRequest("UZS"),
+            extra_return_param: "card",
+            card: { pan: common.visaCard },
+            bank_account: {
+              bank_name: "sberbank",
+              requisite_type: "card",
+            },
+          })
+          .then((p) => p.followFirstProcessingUrl());
+        let err = await response.as_error();
+        err.assert_message(
+          "gateway response error: Not enough money on balance",
+        );
+        await notification;
+      });
     });
-    settings["payout_providers_card"] = true;
-    await merchant.set_settings(settings);
-    let brusnika = ctx.mock_server(BrusnikaPayment.mock_params_uzs(ctx.uuid));
-    brusnika.queue(async (c) => {
-      await delay(950_000);
-      c.status(500);
-      return c.json({});
+
+    test.concurrent("brusnika pending if timeout", async ({ ctx }) => {
+      await ctx.track_bg_rejections(async () => {
+        let merchant = await ctx.create_random_merchant();
+        await merchant.cashin("UZS", common.amount / 100);
+        let settings = providers("UZS", {
+          ...BrusnikaPayout.settings(ctx.uuid),
+          wrapped_to_json_response: true,
+        });
+        settings["payout_providers_card"] = true;
+        await merchant.set_settings(settings);
+        let brusnika = ctx.mock_server(
+          BrusnikaPayout.mock_params_uzs(ctx.uuid),
+        );
+        brusnika.queue(async (c) => {
+          await delay(950_000);
+          c.status(500);
+          return c.json({});
+        });
+        let notification = merchant.queue_notification((callback) => {
+          assert.strictEqual(
+            callback.status,
+            "declined",
+            "declined notification",
+          );
+        });
+        let response = await merchant
+          .create_payout({
+            ...common.payoutRequest("UZS"),
+            extra_return_param: "card",
+            card: { pan: common.visaCard },
+            bank_account: {
+              bank_name: "sberbank",
+              requisite_type: "card",
+            },
+          })
+          .then((p) => p.followFirstProcessingUrl());
+        let err = await response.as_error();
+        err.assert_message(
+          "gateway response error: Not found available payment details",
+        );
+        await notification;
+      });
     });
-    let notification = merchant.queue_notification((callback) => {
-      assert.strictEqual(callback.status, "declined", "declined notification");
-    });
-    let response = await merchant
-      .create_payout({
-        ...common.payoutRequest("UZS"),
-        extra_return_param: "card",
-        card: { pan: common.visaCard },
-        bank_account: {
-          bank_name: "sberbank",
-          requisite_type: "card",
-        },
-      })
-      .then((p) => p.followFirstProcessingUrl());
-    let err = await response.as_error();
-    err.assert_message(
-      "gateway response error: Not found available payment details",
-    );
-    await notification;
   });
-});
