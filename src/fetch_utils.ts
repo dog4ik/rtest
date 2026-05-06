b9f1d32a0bc87e151d051fa96ebb05b0b91cebec
Vadim Gerasimov <angryvadik@gmail.com>
2026-05-06 11:38:09 +0000

    feat: delayed util

--- src/fetch_utils.ts
+++ src/fetch_utils.ts
@@ -1,9 +1,12 @@
 import { z } from "zod";
+import { } from "@std/async"
 import { assert } from "vitest";
 
 export function err_bad_status(response: Response) {
   if (response.status >= 500 && response.status < 600) {
-    assert.fail(`Reactivepay returned bad status code: ${response.statusText} (${response.status})`);
+    assert.fail(
+      `Reactivepay returned bad status code: ${response.statusText} (${response.status})`,
+    );
   }
   return response;
 }
@@ -15,5 +18,12 @@ export function parse_json<T extends z.ZodType>(
     let json = await response.json();
     console.log("Fetch response", json);
     return schema.parse(json);
+  };
+}
+
+export function delayed<T, R>(delay: number, fn: (args: T) => Promise<R>) {
+  return async (args: T): Promise<R> => {
+    await new Promise<void>((resolve) => setTimeout(resolve, delay));
+    return fn(args);
   };
 }
